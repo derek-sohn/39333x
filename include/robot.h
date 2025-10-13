71084c79ca59fea05d7db86484e12a16c4857d4c
derek-sohn <derek.so606@gmail.com>
2025-10-13 15:51:44 -0700

    Update robot.h

--- include/robot.h
+++ include/robot.h
@@ -1,4 +1,6 @@
 #include "main.h"
+#pragma once
+#include "pros/adi.hpp"
 
 
 #ifndef ROBOTH
@@ -11,13 +13,35 @@ extern pros::Motor RF;
 extern pros::Motor RM;
 extern pros::Motor RB;
 extern pros::Motor INTAKE;
+extern pros::Motor INTAKE1;
+extern pros::Motor INTAKE2;
 extern pros::Rotation roto;
-extern pros::Motor LDB;
+extern pros::Rotation roto2;
+extern pros::MotorGroup intakes;
 
 extern pros::Controller con;
 
 extern pros::Imu imu;
 
+extern pros::ADIDigitalOut descore;
+extern pros::ADIDigitalOut descore2;
+extern bool descoreToggle;
+extern bool descore2Toggle;
+
+extern pros::ADIDigitalOut dp1;
+extern pros::ADIDigitalOut dp2;
+extern bool dp1Toggle;
+extern bool dp2Toggle;
+
+extern pros::ADIDigitalOut scrapper;
+extern bool scrapperToggle;
+
+
+extern pros::Optical OpticalC;
+extern pros::Distance DistanceLeft;
+extern pros::Distance DistanceRight;
+
+extern pros::ADIDigitalIn Autonselect;
 
 
 #endif 
